c7e47ca7ff4ee6d84080a20da3e80ef1e318e33d
Kyle Luther <kluther@seungworkstation23.Princeton.EDU>
2018-01-23 19:07:48 -0500

    add option to fill with noise or zeros

--- augment/missing_section.py
+++ augment/missing_section.py
@@ -1,14 +1,23 @@
 import numpy as np
 
-def missing_section_augment(img, p):
+def missing_section_augment(img, p, fill):
   """Performs misalignment augmentation on img.
 
   Args:
     img: (np array: <z,y,x,ch>) image
     p: probability to apply augmentation
+    fill: 'zero' or 'noise'
   """
   if np.random.rand() < p:
-    z = np.random.randint(0, img.shape[0])
-    img[z] = np.zeros_like(img[z])
+    img[z] = missing_section(img[z], fill)
+
+  return img
+
+def missing_section(img, fill):
+  if fill == 'zero':
+    img = np.zeros_like(img)
+  elif fill == 'noise':
+    sigma = np.std(img)
+    img = sigma*np.sqrt(12)*(np.random.rand(*(img.shape))-0.5)
 
   return img
