0bd750ef0e6cbe33b6623cc90732140ce265281e
Kyle Luther <kluther@princeton.edu>
2018-06-15 13:53:08 -0400

    independently choose cirle augmentation in each section

--- augmentem/circle.py
+++ augmentem/circle.py
@@ -10,19 +10,19 @@ def circle_augment(img, p, max_r):
     p: probability to perform augmentation
     max_r: max radius of circle
   """
-  raise NotImplementedError
-  if np.random.rand() < p:
-    z, y, x, ch = img.shape
-    r = np.random.rand() * max_r
-    zc, yc, xc = np.random.randint(z), np.random.randint(y), np.random.randint(x)
-
-    img = circle(img, zc, yc, xc, r)
+  #raise NotImplementedError
+  for z in range(img.shape[0]):
+    if np.random.rand() < p:
+      y, x, ch = img[z].shape
+      r = np.random.rand() * max_r
+      yc, xc = np.random.randint(y), np.random.randint(x)
+      img = circle(img[z], yc, xc, r)
 
   return img
 
-def circle(img, zc, yc, xc, r):
-  z, y, x, ch = img.shape
+def circle(img, yc, xc, r):
+  y, x, ch = img.shape
   yy, xx = draw.circle(yc, xc, r, shape=(y,x))
-  img[zc, yy, xx, :] = 0.0
+  img[yy, xx, :] = 0.0
 
   return img
